4918aa36559c1326c857af179daa131034975344
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-07-07 10:04:33 +0200

    Add template gallery

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,16 @@
 ﻿import Link from "next/link";
+
 import { Navbar } from "./navbar";
+import { TemplateGallery } from "./template-gallery";
 
 const Home = () => {
     return (
         <div className="min-h-screen flex flex-col">
-            <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
+            <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
                 <Navbar />
             </div>
-            <div>
-                Click&nbsp;<Link href="/documents/123">
-                    <span className="text-blue-500 underline">here</span>
-                    &nbsp;to go to document
-                </Link>
+            <div className="mt-16">
+                <TemplateGallery />
             </div>
         </div>
     );
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const templates = [
+    { id: "blank", label: "Blank Document", imageUrl: "/blank-document.svg" },
+    { id: "software-proposal", label: "Software Development Proposal", imageUrl: "/software-proposal.svg" },
+    { id: "project-proposal", label: "Project Development Proposal", imageUrl: "/project-proposal.svg" },
+    { id: "business-letter", label: "Business letter", imageUrl: "/business-letter.svg" },
+    { id: "resume", label: "Resume", imageUrl: "/resume.svg" },
+    { id: "cover-letter", label: "Cover letter", imageUrl: "/cover-letter.svg" },
+    { id: "letter", label: "Letter", imageUrl: "/Letter.svg" },
+];
