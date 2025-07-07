d55256d34437022f960b5b1787c4a61441a423e6
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-07-07 10:30:41 +0200

    Add convex

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,19 @@
-﻿import Link from "next/link";
+﻿"use client";
+
+import Link from "next/link";
 
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+    const documents = useQuery(api.documents.get);
+
+    if (!documents) {
+        return <div>Loading...</div>;
+    };
+    
     return (
         <div className="min-h-screen flex flex-col">
             <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -11,6 +21,9 @@ const Home = () => {
             </div>
             <div className="mt-16">
                 <TemplateGallery />
+                {documents?.map((document) => (
+                    <span key={document._id}>{document.title}</span>
+                ))}
             </div>
         </div>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { ConvexClientProvider } from "@/components/ui/convex-client-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +22,9 @@ export default function RootLayout({
         className={inter.className}
       >
         <NuqsAdapter>
-          {children}
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
         </NuqsAdapter>
       </body>
     </html>
